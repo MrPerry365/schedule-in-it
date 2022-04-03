eaebea8c502f664ac8abbc9b31c6fd384b40f239
MrPerry365 <1987nowhere@gmail.com>
2022-04-03 12:06:29 -0400

    add more detail to comment

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-// AS AN employee with a busy schedule
-// I WANT to add important events to a daily planner
-// SO THAT I can manage my time effectively
+// AS AN employee with a busy schedule //
+// I WANT to add important events to a daily planner //
+// SO THAT I can manage my time effectively //
 
-// GIVEN I am using a daily planner to create a schedule
+// GIVEN I am using a daily planner to create a schedule //
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
+// WHEN I open the planner //
+// THEN the current day is displayed at the top of the calendar //
     moment().format('MMMM Do YYYY, h:mm:ss a');
 
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
+// WHEN I scroll down //
+// THEN I am presented with time blocks for standard business hours //
 // - add column to host listed time, add user input, and add button to save input //
 
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// add box shadow and color to listed items when targeted
+// WHEN I view the time blocks for that day //
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future //
+// add box shadow and color to listed items when targeted //
+// - add function to track each hour //
+// - add loop to check if hour is past, present, or future //
 
-// WHEN I click into a time block
-// THEN I can enter an event
+// WHEN I click into a time block //
+// THEN I can enter an event //
+// -add textarea to html //
 
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// -
+// WHEN I click the save button for that time block //
+// THEN the text for that event is saved in local storage //
+// - add function to save event to local storage //
 
-// WHEN I refresh the page
-// THEN the saved events persist
+// WHEN I refresh the page //
+// THEN the saved events persist //
 // -
