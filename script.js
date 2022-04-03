a324f43ff26bafb1f83335af3cee4221f1ab42f5
MrPerry365 <1987nowhere@gmail.com>
2022-04-03 09:50:44 -0400

    add commments

---
+++ script.js
@@ -0,0 +1,28 @@
+// AS AN employee with a busy schedule
+// I WANT to add important events to a daily planner
+// SO THAT I can manage my time effectively
+
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+    moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+// - add column to host listed time, add user input, and add button to save input //
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// add box shadow and color to listed items when targeted
+
+// WHEN I click into a time block
+// THEN I can enter an event
+
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// -
+
+// WHEN I refresh the page
+// THEN the saved events persist
+// -
