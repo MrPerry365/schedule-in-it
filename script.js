8e819360ad57e60d509facae5fe311a421c108fb
MrPerry365 <1987nowhere@gmail.com>
2022-04-03 18:51:00 -0400

    add current time and adjust script loading

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // AS AN employee with a busy schedule //
 // I WANT to add important events to a daily planner //
 // SO THAT I can manage my time effectively //
-
+$(document).ready(function() {
 // GIVEN I am using a daily planner to create a schedule //
+
 
 // WHEN I open the planner //
 // THEN the current day is displayed at the top of the calendar //
-    moment().format('MMMM Do YYYY, h:mm:ss a');
+   $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-// WHEN I scroll down //
-// THEN I am presented with time blocks for standard business hours //
-// - add column to host listed time, add user input, and add button to save input //
+
 
 // WHEN I view the time blocks for that day //
 // THEN each time block is color-coded to indicate whether it is in the past, present, or future //
@@ -18,14 +17,13 @@
 // - add function to track each hour //
 // - add loop to check if hour is past, present, or future //
 
-// WHEN I click into a time block //
-// THEN I can enter an event //
-// -add textarea to html //
+
 
 // WHEN I click the save button for that time block //
 // THEN the text for that event is saved in local storage //
-// - add function to save event to local storage //
+// - add function to listen to button and save event to local storage //
 
 // WHEN I refresh the page //
 // THEN the saved events persist //
-// -
+// -} 
+} )
