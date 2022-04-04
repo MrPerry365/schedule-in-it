665ca5e534157ef3458fb9c52d90e99fc43b0396
MrPerry365 <1987nowhere@gmail.com>
2022-04-03 21:10:11 -0400

    defined save button

--- script.js
+++ script.js
@@ -22,7 +22,16 @@ $(document).ready(function() {
 // WHEN I click the save button for that time block //
 // THEN the text for that event is saved in local storage //
 // - add function to listen to button and save event to local storage //
+$(".saveBtn").on ("click", function () {
+var text = $(this).siblings(".description").val();
+console.log(text);
+var time = $(this).parent().attr("id");
+console.log(time);
 
+localStorage.setItem(text, time);
+ 
+    
+})
 // WHEN I refresh the page //
 // THEN the saved events persist //
 // -} 
